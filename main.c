621f3f03a9430804d8fb8515dd5ca556169aa561
Muzaffar Khaydarov <114870653+muzaffarcodes@users.noreply.github.com>
2024-01-03 21:45:20 +0500

    Add files via upload

---
+++ main.c
@@ -0,0 +1,130 @@
+/*
+#include <stdio.h>
+
+int main()
+{
+	unsigned int num = -2;
+	printf("%u",num);
+
+	//Qoshish son++ ++son son+=10 son = son + 10
+	//Ayirish son-- --son son-=10 son = son - 10
+	//Kopaytirish         son*=10 son = son * 10
+	//Bolish              son/=10 son = son / 10
+	//Mod                 son%=10 son = son % 10
+
+	int son = 10;
+	son++;
+	printf("%d\n", son++);
+	printf("%d\n",++son);
+
+	int son3 = 20;
+	son3 += 20;
+	printf("%d\n", son);
+
+	int num1 = 20;
+	num1 /= 20;
+	printf("%d\n", num1);
+
+	int son1 = 4;
+	int son2 = son1++;
+	int son3 = ++son1;
+
+	printf("%d %d %d\n", son1,son2,son3);
+
+	int numMod = 100;
+	numMod %= 5;
+	printf("%d\n", numMod);
+
+	int raqam = 100;
+	raqam *= 5;
+	printf("%d\n", raqam);
+
+	int jami = 10;
+	printf("%d\n", jami++ + ++jami);
+	printf("%d\n", jami++ + jami++);
+
+	int son = 100;
+	printf("Son: %d", son + 100);
+	printf("%d", son);
+
+	return 0;
+}
+#include <stdio.h>
+
+int main()
+{
+    float a,b;
+    printf("Iltimos oldin a ning keyin b ning qiymatini kiriting!");
+    scanf("%f%f", &a,&b);
+    printf("Kvadrat: %.2f", a*b);
+
+    return 0;
+}
+
+
+#include <stdio.h>
+//Uy ishi bir - Aylana radiusi//Diametri formulasi
+
+int main()
+{
+        int d,Radius;
+        printf("Aylana Radiusini kiriting: ");
+        scanf("%d", &Radius);
+        printf("Javob: %d", 2*Radius);
+        return 0;
+}
+*/
+//Uy ishi ikki - kubning hajmi//
+/*
+int main()
+{
+        int V,a,b,c;
+        printf("Iltimos kubni uzunligini kiriting: ");
+        scanf("%d", &a);
+        printf("Iltimos kubni enini kiriting: ");
+        scanf("%d", &b);
+        printf("Iltimos kubni bo'yini kiriting: ");
+        scanf("%d", &c);
+        V = a*b*c;
+        printf("Javob: %d", a*b*c);
+        return 0;
+}
+*/
+//Uy ishi uch Doira maydoni//
+/*
+int main()
+{
+    float A,p,r;
+    printf("Radiusni kiriting: ");
+    scanf("%f", &r);
+    p = 3.14;
+    printf("Javob: %.2f",p*r*r);
+    return 0;
+}
+
+//Uy ishi to'rt Sektor yuzasi//
+
+
+int main()
+{
+    float S,Radius,abutech;
+    printf("Radiusni kiriting: ");
+    scanf("%f", &Radius);
+
+    printf("a ni kiriting: ");
+    scanf("%f", &abutech);
+    printf("Javobingiz: %f", 0.5*Radius*Radius*abutech);
+    return 0;
+}
+
+#include <stdio.h>
+
+int main()
+{
+    int son = 0;
+    printf("%d\n", son++);
+    printf("%d\n", ++son);
+    return 0;
+
+}
+*/
